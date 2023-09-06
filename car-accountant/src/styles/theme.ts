ffd88c72873a6b8f609d0bddf30811d462b0c490
GeorgiDishkov-Enter21 <60046070+GeorgiDishkov@users.noreply.github.com>
2023-09-06 22:07:42 +0300

    Change scroll color and size

--- car-accountant/src/styles/theme.ts
+++ car-accountant/src/styles/theme.ts
@@ -32,6 +32,40 @@ const theme = createTheme({
       textTransform: 'none',
     },
   },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          scrollbarColor: '#F3F8FF',
+          '&::-webkit-scrollbar, & *::-webkit-scrollbar': {
+            backgroundColor: '#F3F8FF',
+            height: '0.5rem',
+            width: '0.5rem',
+          },
+          '&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb': {
+            borderRadius: 4,
+            backgroundColor: '#173475',
+          },
+          '&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus': {
+            backgroundColor: '#173475',
+          },
+          '&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active': {
+            backgroundColor: '#173475',
+          },
+          '&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#173475',
+          },
+          '&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner': {
+            backgroundColor: '#173475',
+          },
+        },
+        html: {
+          WebkitFontSmoothing: 'auto',
+          scrollBehavior: 'smooth',
+        },
+      },
+    },
+  },
 })
 
 export default theme
